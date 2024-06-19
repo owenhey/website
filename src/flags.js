e3ccf202ddc3b9e3d3be19bdcb19b6b2471e0071
Owen Hey <oysterhey@gmail.com>
2024-06-18 23:21:17 -0400

    flag grid and parsing

--- src/flags.js
+++ src/flags.js
@@ -23,25 +23,65 @@ fetch('flags.csv')
   .then(response => response.text())
   .then(data => {
     parsedData = parseCSV(data);
-    showFlag();
   })
   .catch(error => {
     console.error('Error reading file:', error);
 });
 
-function showFlag(){
+const numberInput = document.querySelector('.flags-number');
+const flagList = [];
+
+function showRandomFlag(element){
   // const randomIndex = 132;
   const randomIndex = Math.floor(Math.random()*parsedData.length);
   const flagData = parsedData[randomIndex];
 
-  const flagSvg = document.getElementById('flag-svg');
+  const flagSvg = element.querySelector('#flag-svg');
   flagSvg.setAttribute('xlink:href', flagData.Flag_image_url);
 
-  const countryName = document.getElementById('country-name');
+  const countryName = element.querySelector('#country-name');
   countryName.textContent = flagData.Country;
 }
 
 const nextFlagButton = document.getElementById('next-flag-button');
 nextFlagButton.addEventListener("click", ()=>{
-  showFlag();
+  const numFlagsToShow = numberInput.value;
+  // Set them hidden or not
+  for (let i = 0; i < flagList.length; i++) {
+    const shouldShow = i < numFlagsToShow;
+    if(shouldShow){
+      flagList[i].style.display = 'flex';
+      showRandomFlag(flagList[i]);
+    }
+    else{
+      flagList[i].style.display = 'none';
+    }
+  }
 })
+
+
+numberInput.addEventListener('input', function() {
+  if(this.value === ''){
+    this.value = '';
+    return;
+  }
+  if(this.value === 'e' || this.value === 'E'){
+    this.value = '';
+    return;
+  }
+  const enteredValue = parseInt(this.value);
+  const clampedValue = Math.min(Math.max(enteredValue, 2), 8);
+  numberInput.value = clampedValue;
+});
+
+const baseFlag = document.querySelector('.flag-container');
+const parent = document.querySelector('.flags');
+
+flagList.push(baseFlag);
+
+// Create copies of the base element and append them to the parent
+for (let i = 0; i < 7; i++) {
+  const copy = baseFlag.cloneNode(true);
+  parent.appendChild(copy);
+  flagList.push(copy);
+}
