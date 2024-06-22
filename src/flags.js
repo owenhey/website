61a6fa3b1aea5c9e5733773dc12323076a26d28c
Owen Hey <oysterhey@gmail.com>
2024-06-22 16:20:30 -0400

    flags done

--- src/flags.js
+++ src/flags.js
@@ -1,9 +1,21 @@
-let parsedData;
-const numberInput = document.querySelector('.flags-number');
-const flagList = [];
+let allFlagData;
 let gameMode = 0; // 0 for pick flag, 1 for pick name
 let correctIndex;
+let questionFlagElement;
 
+const flagList = [];
+let answerMap = [];
+let streak = 0;
+
+const numberInput = document.querySelector('.flags-number');
+const streakText = document.querySelector('.flags-streak-text');
+
+// Allow scrolling on this page
+document.body.style.overflow = 'scroll';
+document.querySelector('.main-content').style.height = 'auto';
+document.querySelector('.main-content').style.minHeight = '135vh';
+
+// Goes through flags.csv and grabs all the flag data from it
 function parseCSV(csv) {
   const lines = csv.trim().split('\n');
   const headers = lines[0].split(',');
@@ -23,33 +35,60 @@ function parseCSV(csv) {
   return result;
 }
 
-function checkAnswer(chosenIndex) {
-  const responseText = document.getElementById('flags-response-text');
-  responseText.style.display = 'block';
-  if (chosenIndex === correctIndex) {
-    responseText.innerHTML = "Correct!";
-    generateQuestion();
-  } else {
-    responseText.innerHTML = "That's not correct";
-  }
+{ // This actually fetches the CSV, is a stalling call
+  fetch('flags.csv')
+  .then(response => response.text())
+  .then(data => {
+    allFlagData = parseCSV(data);
+    generateQuestion(); // Generate the first question when finished
+  })
+  .catch(error => {
+    console.error('Error reading file:', error);
+});
 }
 
+// Creates the 8 flag possiblities as well as the "question flag"
 function generateElements(){
   const baseFlag = document.querySelector('.flag-container');
-  const parent = document.querySelector('.flags');
+  const parent = document.querySelector('.flag-answers');
   baseFlag.setAttribute('data-index', '0');
 
   flagList.push(baseFlag);
 
-  // Create copies of the base element and append them to the parent
+  // Create the 8 answer copies
   for (let i = 1; i < 8; i++) {
     const copy = baseFlag.cloneNode(true);
     copy.setAttribute('data-index', i.toString());
     parent.appendChild(copy);
     flagList.push(copy);
   }
+
+  // Create the base copy
+  questionFlagElement = baseFlag.cloneNode(true);
+  document.getElementById('flag-question-container').appendChild(questionFlagElement);
+  questionFlagElement.style.display = 'none';
+  // Remove the text and button
+  questionFlagElement.querySelector('.country-name').remove();
+  questionFlagElement.querySelector('.button').remove();
 }
 
+// Called when a guess is chosen
+function checkAnswer(chosenIndex) {
+  const responseText = document.getElementById('flags-response-text');
+  responseText.style.display = 'block';
+  if (chosenIndex === correctIndex) {
+    responseText.innerHTML = "Correct!";
+    generateQuestion();
+    streak++;
+  } else {
+    console.log(answerMap);
+    responseText.innerHTML = "Wrong, that's " + answerMap[chosenIndex].Country;
+    streak = 0;
+  }
+  streakText.innerHTML = "Streak: " + streak;
+}
+
+// Adds listeners to all the answer buttons
 function setupChooseButtons() {
   const chooseButtons = document.querySelectorAll('#flags-choose-button');
   chooseButtons.forEach(button => {
@@ -61,16 +100,17 @@ function setupChooseButtons() {
   });
 }
 
+// Generates random flags and then selects one to be the correct answer
 function generateQuestion() {
   const numFlags = parseInt(numberInput.value);
-  const uniqueFlags = getUniqueFlags(numFlags);
+  answerMap = getUniqueFlags(numFlags);
 
   // Set the flags to be displayed
   for (let i = 0; i < flagList.length; i++) {
     const shouldShow = i < numFlags;
     if (shouldShow) {
       flagList[i].style.display = 'flex';
-      showFlag(flagList[i], uniqueFlags[i]);
+      showFlag(flagList[i], answerMap[i]);
     } else {
       flagList[i].style.display = 'none';
     }
@@ -78,25 +118,30 @@ function generateQuestion() {
 
   // Choose a random flag to ask the question about
   correctIndex = Math.floor(Math.random() * numFlags);
-  const questionFlag = uniqueFlags[correctIndex];
+  const questionFlag = answerMap[correctIndex];
   
   const questionText = document.getElementById('flags-question-text');
   if(gameMode === 0){
-    questionText.innerHTML = 'Which flag is ' + questionFlag.Country + '?';
+    questionText.textContent = 'Which flag is ' + questionFlag.Country + '?';
+    questionFlagElement.style.display = 'none';
   }
   else{
-    
+    questionText.textContent = 'Which state is';
+    questionFlagElement.style.display = 'block';
+    const questionFlagSvg = questionFlagElement.querySelector('#flag-svg');
+    questionFlagSvg.setAttribute('src', questionFlag.Flag_image_url);
   }
 }
 
+// Grabs a list of unique flags, no duplicates allowed
 function getUniqueFlags(numFlags) {
   const uniqueFlags = [];
   const usedIndexes = new Set();
 
   while (uniqueFlags.length < numFlags) {
-    const randomIndex = Math.floor(Math.random() * parsedData.length);
+    const randomIndex = Math.floor(Math.random() * allFlagData.length);
     if (!usedIndexes.has(randomIndex)) {
-      uniqueFlags.push(parsedData[randomIndex]);
+      uniqueFlags.push(allFlagData[randomIndex]);
       usedIndexes.add(randomIndex);
     }
   }
@@ -104,19 +149,7 @@ function getUniqueFlags(numFlags) {
   return uniqueFlags;
 }
 
-
-{ // Actually fetch it
-  fetch('flags.csv')
-  .then(response => response.text())
-  .then(data => {
-    parsedData = parseCSV(data);
-    generateQuestion();
-  })
-  .catch(error => {
-    console.error('Error reading file:', error);
-});
-}
-
+// Just makes sure the number input only allows 2-8
 function setupNumberInput(){
   numberInput.addEventListener('input', function() {
     if(this.value === ''){
@@ -133,6 +166,7 @@ function setupNumberInput(){
   });
 }
 
+// Displays a flag in a given flag element
 function showFlag(element, flagData){
   const flagSvg = element.querySelector('#flag-svg');
   flagSvg.setAttribute('src', flagData.Flag_image_url);
@@ -140,16 +174,17 @@ function showFlag(element, flagData){
   const countryName = element.querySelector('#country-name');
   countryName.textContent = flagData.Country;
 
-  if(gameMode === 0){
+  if(gameMode === 0){ // Meaning have to click on a flag
     countryName.style.display = 'none';
     flagSvg.style.display = 'block';
   }
-  else{
+  else{ // Meaning have to click on a name
     countryName.style.display = 'block';
     flagSvg.style.display = 'none';
   }
 }
 
+// Setup the game mode button
 const flagGameMode = document.getElementById('flags-game-mode');
 flagGameMode.addEventListener("click", ()=>{
   if(gameMode === 0){
@@ -162,7 +197,7 @@ flagGameMode.addEventListener("click", ()=>{
   }
 })
 
-
+// Actually call all the methods for setup
 setupNumberInput();
 generateElements();
 setupChooseButtons();
