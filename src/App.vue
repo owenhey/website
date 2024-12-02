f0fae99568508fa7e251248d80f52718ed58c55c
Owen Hey <oysterhey@gmail.com>
2024-12-02 18:03:16 -0500

    vue switch base

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="background"></div>
+	<div class="background-grid"></div>
+	<div class="background-grid-2"></div>
+	<RouterView />
+</template>
+
+<script lang="ts">
+	import { ref, onMounted, computed } from 'vue';
+	import { useRoute, useRouter } from 'vue-router';
+
+	const appDiv = ref<HTMLElement | null>(null);
+	const appBody = ref<HTMLElement | null>(null);
+
+	export default {
+        setup() {
+			const router = useRouter()
+			const route = useRoute()
+			const assetImport = import.meta.glob("./assets/**/*"); // Force imports everything in the assets folder
+
+			return {
+                router
+            }
+		}
+	}
+</script>
---
+++ src/views/title/HomeScreen.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="center-main-content">
+		<main class="main-content">
+			<nav class="header">
+				<ul class="header-list">
+					<button class="header-button">home</button>
+					<button class="header-button">portfolio</button>
+					<button class="header-button">blog</button>
+					<button class="header-button">flags</button>
+					<button class="header-button">about</button>
+				</ul>
+			</nav>
+			<div class="scrolling-content">
+				<h1>welcome to my website</h1>
+				<p>here is some information about me</p>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script lang='ts'>
+	import '@/assets/base.css'
+
+	import { onMounted } from 'vue'
+	import { useRouter } from 'vue-router'
+
+	export default{
+		setup(){
+			const router = useRouter()
+
+			onMounted(() => {
+				
+			})
+
+			return{
+				router
+			}
+		}
+	}
+</script>
